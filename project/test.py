b07ce0d4a8eee5df78b7e6c7291d7c67b05b0313
umluizlima <umluizlima@gmail.com>
2018-12-24 19:56:17 -0200

    Add fraction handling test to custom sum()

--- project/test.py
+++ project/test.py
@@ -1,7 +1,7 @@
 import unittest
-
 # Import sum() from the my_sum package
 from my_sum import sum
+from fractions import Fraction
 
 
 # Define a new test case class
@@ -15,6 +15,14 @@ class TestSum(unittest.TestCase):
         data = [1, 2, 3]
         result = sum(data)
         self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
 
 
 # Define a command-line entry point
