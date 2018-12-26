b74c5f6f32532d9fc06f8e6811c948760a23d6b8
umluizlima <umluizlima@gmail.com>
2018-12-26 12:09:22 -0200

    Add exception handling test

--- project/test.py
+++ project/test.py
@@ -16,13 +16,29 @@ class TestSum(unittest.TestCase):
         result = sum(data)
         self.assertEqual(result, 6)
 
+    # Every test follows 3 basic steps
     def test_list_fraction(self):
         """
         Test that it can sum a list of fractions
         """
+        # Create your inputs
         data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        # Execute the code, capturing the output
         result = sum(data)
+        # Compare the output with an expected result
         self.assertEqual(result, 1)
+
+    # It is possible to handle expected failures in tests
+    def test_bad_type(self):
+        """
+        Test that it throws a TypeError when given bad input
+        """
+        # Create an input that should raise an error when used
+        data = "banana"
+        # .assertRaises() is used as a context-manager
+        with self.assertRaises(TypeError):
+            # The output is captured within the with block
+            result = sum(data)
 
 
 # Define a command-line entry point
