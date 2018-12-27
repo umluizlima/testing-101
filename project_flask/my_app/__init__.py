c8b685d889f2da3fd4f65ec275db3784966e6e58
umluizlima <umluizlima@gmail.com>
2018-12-27 17:24:07 -0200

    Add configuration settings to create_app()

--- project_flask/my_app/__init__.py
+++ project_flask/my_app/__init__.py
@@ -1,11 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
+from tinydb import TinyDB
 
 
-def create_app():
+def create_app(config=None):
+    
     app = Flask(__name__)
+    app.config.from_object(config or os.environ['APP_SETTINGS'])
+
+    db = TinyDB(app.config['DATABASE'])
 
     @app.route("/")
     def home():
         return "Howdy from home!"
+
+    @app.route("/fruits", methods=["GET"])
+    def list_fruits():
+        return jsonify(db.all())
 
     return app
